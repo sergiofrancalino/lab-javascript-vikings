7e4726081796d69dca1449ef986cf489a68a7258
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-10-24 16:44:39 -0300

    fazendo

--- src/viking.js
+++ src/viking.js
@@ -2,10 +2,17 @@
 class Soldier {}
 
 // Viking
-class Viking {}
-
+class Viking extends Soldir {}
+construtor (name, health, )
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+const war1 = new War()
+const viking1 = new Viking('Ragnar', 100, 150);
+const saxon1 = new Sexon(150, 100);
+viking1.attack()
+
+war1.addViking(viking1)
+war1.addSaxon(saxon1)
