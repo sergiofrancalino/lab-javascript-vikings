d4601f37d05f57369e2caac804fd9ef100cd3da8
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-10-24 20:02:04 -0300

    fazendo

--- src/viking.js
+++ src/viking.js
@@ -2,12 +2,13 @@
 class Soldier {
 constructor(health, strength){
     this.health= health;
-    this.strength= strength;}
+    this.strength = strength;}
 attack(){return this.strength;}
 receiveDamage(damage){this.health -= damage;}
 }
 
 // Viking
+
 class Viking extends Soldier {
     constructor(name,health, strength){
         super(health, strength);
@@ -15,16 +16,19 @@ class Viking extends Soldier {
     }
     receiveDamage(damage){
         this.health -= damage;
-        return this.health > 0? `${this.name} has received ${damage} points of damage`: `${this.name} died in act of combat`;
-
+        return this.health > 0? `${this.name} has received ${damage} points of damage`:`${this.name} died in act of combat`;
     }
     battleCry(){
         return `Odin Owns You All`;
     }
 }
-
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    receiveDamage(damage){
+        super.receiveDamage(damage);
+        return this.health > 0 ? `A Saxon has received ${damage} points of damage`: `A Saxon has died in combat`
+    }
+}
 
 // War
 class War {}
